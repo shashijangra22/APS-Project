5f1fdeb8a13e61fa76e5154cd9a680671c6a1d92
Varun Gupte <varun.gpt1007@gmail.com>
2018-10-30 00:44:34 +0530

    Update binomial.cpp

--- binomial.cpp
+++ binomial.cpp
@@ -1,231 +1,244 @@
-/*
- * C++ Program to Implement Binomial Heap
- */
-#include <iostream>
-#include <cstdlib>
+#include<stdio.h>
+#include<malloc.h>
+
+#include<bits/stdc++.h>
+
 using namespace std;
-/*
- * Node Declaration
- */
-struct node
+
+
+//node structure
+struct node 
 {
     int n;
     int degree;
-    node* parent;
-    node* child;
-    node* sibling;
-};
-/*
- * Class Declaration
- */
-class BinomialHeap
+    struct node* parent;
+    struct node* child;
+    struct node* sibling;
+}*H=NULL,*Hr=NULL;
+
+
+//node creation 
+struct node* create_node(int k) 
 {
-    private:
-        node *H;
-        node *Hr;
-        int count;
-    public:
-        node* Initializeheap();
-        int Binomial_link(node*, node*);
-        node* Create_node(int);
-        node* Union(node*, node*);
-        node* Insert(node*, node*);
-        node* Merge(node*, node*);
-        node* Extract_Min(node*);
-        int Revert_list(node*);
-        int Display(node*);
-        node* Search(node*, int);
-        int Decrease_key(node*, int, int);
-        int Delete(node*, int);
-        BinomialHeap()
-        {
-            H = Initializeheap();
-            Hr = Initializeheap();
-            int count = 1;
-        }
-};
- 
-/*
- * Initialize Heap
- */
-node* BinomialHeap::Initializeheap()
+    struct node* p;
+    p=new node;
+    p->n = k;
+    return p;
+}
+
+
+//heap initialisation 
+struct node* initialise_heap() 
 {
-    node* np;
+    struct node* np;
     np = NULL;
     return np;
 }
-/*
- * Linking nodes in Binomial Heap
- */
-int BinomialHeap::Binomial_link(node* y, node* z)
+
+
+// print function for Binomial Tree 
+void printTree(node *h) 
+{ 
+    while (h) 
+    { 
+        cout << h->n << " "; 
+        printTree(h->child); 
+        h = h->sibling; 
+    }
+  //  cout<<endl; 
+} 
+struct node* simply_merging(struct node* H1, struct node* H2) 
+{
+
+    struct node* H = initialise_heap();
+    struct node* y;
+    struct node* z;
+    struct node* a;
+    struct node* b;
+    y = H1;
+    z = H2;
+   
+        if (y!=NULL && z != NULL && y->degree <= z->degree)
+            H = y;
+        else if (y!=NULL && z != NULL && y->degree > z->degree)
+            H = z;
+        else if (z==NULL )
+            H = y;
+        else
+            H = z;
+    while (y != NULL && z != NULL) 
+    {
+        if (y->degree < z->degree) 
+        {
+            y = y->sibling;
+        }
+        else if(z->sibling && z->degree<z->sibling->degree && z->sibling->degree < y->degree)
+        {
+           z = z->sibling;  
+        } 
+        else if (y->degree == z->degree) 
+        {
+            a = y->sibling;
+            y->sibling = z;
+            y = a;
+        } 
+        else 
+        {
+            b = z->sibling;
+            z->sibling = y;
+            z = b;
+        }
+    }   
+    return H;
+}
+
+//merging two same degree nodes
+int linking_same_degnodes(struct node* y, struct node* z) 
 {
     y->parent = z;
     y->sibling = z->child;
     z->child = y;
     z->degree = z->degree + 1;
 }
-/*
- * Create Nodes in Binomial Heap
- */
-node* BinomialHeap::Create_node(int k)
+ 
+
+// union of two heaps 
+struct node* union_of_heaps (struct node* H1, struct node* H2) 
 {
-    node* p = new node;
-    p->n = k;
-    return p;
+   // cout<<"union_of_heaps";
+    
+    struct node* prev_x;
+    struct node* next_x;
+    struct node* x;
+    struct node* H = initialise_heap();
+    // struct node* H = new node;
+    H = simply_merging(H1, H2);
+    if (H == NULL)
+        return H;
+   
+    prev_x = NULL;
+    x = H;
+    next_x = x->sibling;
+   
+    while (next_x != NULL) 
+    {
+        if ((x->degree != next_x->degree) ) 
+        {
+            prev_x = x;
+            x = next_x;
+        } 
+        else if ((x->degree == next_x->degree) && (next_x->sibling != NULL) && (next_x->sibling)->degree == x->degree)
+        {
+             prev_x = x;
+             x = next_x;
+        }
+        else 
+        {
+            if (x->n <= next_x->n) 
+            {
+                x->sibling = next_x->sibling;
+                linking_same_degnodes(next_x, x);
+            } 
+            else 
+            {
+                if (prev_x == NULL)
+                { 
+                    H = next_x;
+                }
+                else
+                {
+                    prev_x->sibling = next_x;
+                }
+
+                linking_same_degnodes(x, next_x);
+                x = next_x;
+            }
+        }
+        
+        next_x = x->sibling;
+    }
+    return H;
 }
-/*
- * Insert Nodes in Binomial Heap
- */
-node* BinomialHeap::Insert(node* H, node* x)
+
+//insert operation 
+struct node* insert(struct node* H, struct node* x) 
 {
-    node* H1 = Initializeheap();
+     struct node* H1 = initialise_heap();
+    // struct node* H1 = new node;
+
     x->parent = NULL;
     x->child = NULL;
     x->sibling = NULL;
     x->degree = 0;
     H1 = x;
-    H = Union(H, H1);
+    H = union_of_heaps(H, H1);
     return H;
 }
- 
-/*
- * Union Nodes in Binomial Heap
- */
-node* BinomialHeap::Union(node* H1, node* H2)
+
+
+//printing only root elements 
+int display_root_elements(struct node* H) 
 {
-    node *H = Initializeheap();
-    H = Merge(H1, H2);
-    if (H == NULL)
-        return H;
-    node* prev_x;
-    node* next_x;
-    node* x;
-    prev_x = NULL;
-    x = H;
-    next_x = x->sibling;
-    while (next_x != NULL)
-    {
-        if ((x->degree != next_x->degree) || ((next_x->sibling != NULL)
-            && (next_x->sibling)->degree == x->degree))
-        {
-            prev_x = x;
-            x = next_x;
-        }
-        else
-	    {
-            if (x->n <= next_x->n)
-            {
-                x->sibling = next_x->sibling;
-                Binomial_link(next_x, x);
-            }
-            else
-            {
-                if (prev_x == NULL)
-                    H = next_x;
-                else
-                    prev_x->sibling = next_x;
-                Binomial_link(x, next_x);
-                x = next_x;
-            }
-	    }
-        next_x = x->sibling;
-    }
-    return H;
-}
-/*
- * Merge Nodes in Binomial Heap
- */
-node* BinomialHeap::Merge(node* H1, node* H2)
-{
-    node* H = Initializeheap();
-    node* y;
-    node* z;
-    node* a;
-    node* b;
-    y = H1;
-    z = H2;
-    if (y != NULL)
-    {
-        if (z != NULL)
-        {
-            if (y->degree <= z->degree)
-                H = y;
-            else if (y->degree > z->degree)
-                H = z;
-        }
-        else
-            H = y;
-    }
-    else
-        H = z;
-    while (y != NULL && z != NULL)
-    {
-        if (y->degree < z->degree)
-        {
-            y = y->sibling;
-        }
-        else if (y->degree == z->degree)
-        {
-            a = y->sibling;
-            y->sibling = z;
-            y = a;
-        }
-        else
-        {
-            b = z->sibling;
-            z->sibling = y;
-            z = b;
-        }
-    }
-    return H;
-}
-/*
- * Display Binomial Heap
- */
-int BinomialHeap::Display(node* H)
-{
-    if (H == NULL)
-    {
-        cout<<"The Heap is empty"<<endl;
+    struct node* p;
+    if (H == NULL) {
+        printf("\nheap is empty");
         return 0;
     }
-    cout<<"The root nodes are: "<<endl;
-    node* p;
+    printf("\nroots are-\n");
     p = H;
-    while (p != NULL)
-    {
-        cout<<p->n;
+    while (p != NULL) {
+        printf("%d", p->n);
+        cout<<"("<<p->degree<<")";
         if (p->sibling != NULL)
-            cout<<"-->";
+            printf("-->");
         p = p->sibling;
     }
-    cout<<endl;
+    printf("\n");
 }
-/*
- * Extract Minimum
- */
-node* BinomialHeap::Extract_Min(node* H1)
+ 
+//reverting children in list once root deleted 
+void reverting_list(struct node* y) 
 {
-    Hr = NULL;
-    node* t = NULL;
-    node* x = H1;
-    if (x == NULL)
+    if (y->sibling != NULL) {
+        reverting_list(y->sibling);
+        (y->sibling)->sibling = y;
+    } 
+    else 
     {
-        cout<<"Nothing to Extract"<<endl;
+        Hr = y;
+    }
+}
+struct node* extract_min_node(struct node* H1) 
+{
+    int min;
+    struct node* t = NULL;
+    struct node* x = H1;
+    //struct node *Hr;
+    struct node* p;
+  
+    Hr = NULL;
+    if (x == NULL) 
+    {
+        printf("\nNode not found");
         return x;
     }
-    int min = x->n;
-    node* p = x;
-    while (p->sibling != NULL)
+
+    p = x;
+    
+    min=p->n;
+
+    while (p->sibling != NULL) 
     {
-        if ((p->sibling)->n < min)
+        if ((p->sibling)->n < min) 
         {
             min = (p->sibling)->n;
             t = p;
             x = p->sibling;
         }
+
         p = p->sibling;
     }
+    
     if (t == NULL && x->sibling == NULL)
         H1 = NULL;
     else if (t == NULL)
@@ -234,157 +247,194 @@ node* BinomialHeap::Extract_Min(node* H1)
         t = NULL;
     else
         t->sibling = x->sibling;
-    if (x->child != NULL)
+
+
+   
+    
+    if (x->child != NULL) 
     {
-        Revert_list(x->child);
+        reverting_list(x->child);
         (x->child)->sibling = NULL;
+
     }
-    H = Union(H1, Hr);
+     if(!H1)
+     {
+        swap(H1,Hr);
+     }
+     H = union_of_heaps(H1, Hr);
     return x;
 }
-/*
- * Reverse List
- */
-int BinomialHeap::Revert_list(node* y)
-{
-    if (y->sibling != NULL)
-    {
-        Revert_list(y->sibling);
-        (y->sibling)->sibling = y;
-    }
-    else
-    {
-        Hr = y;
-    }
-}
  
-/*
- * Search Nodes in Binomial Heap
- */
-node* BinomialHeap::Search(node* H, int k)
+struct node* search_element(struct node* H, int k) 
 {
-    node* x = H;
-    node* p = NULL;
-    if (x->n == k)
-    {
+    struct node* x = H;
+    struct node* p = NULL;
+    if (x->n == k) {
         p = x;
         return p;
     }
-    if (x->child != NULL && p == NULL)
-        p = Search(x->child, k);
-    if (x->sibling != NULL && p == NULL)
-        p = Search(x->sibling, k);
+    if (x->child != NULL && p == NULL) {
+        p = search_element(x->child, k);
+    }
+ 
+    if (x->sibling != NULL && p == NULL) {
+        p = search_element(x->sibling, k);
+    }
     return p;
 }
-/*
- * Decrease key of a node
- */
-int BinomialHeap::Decrease_key(node* H, int i, int k)
+ 
+int decrease_key(struct node* H, int i, int k) 
 {
     int temp;
-    node* p;
-    node* y;
-    node* z;
-    p = Search(H, i);
-    if (p == NULL)
-    {
-        cout<<"Invalid choice of key"<<endl;
+    struct node* p;
+    struct node* y;
+    struct node* z;
+    p = search_element(H, i);
+    if (p == NULL) {
+        printf("\ninvalid choice\n");
         return 0;
     }
-    if (k > p->n)
-    {
-        cout<<"Error!! New key is greater than current key"<<endl;
+    if (k > p->n) {
+        printf("\nnew key cannot be greater than previous key ");
         return 0;
     }
     p->n = k;
     y = p;
     z = p->parent;
-    while (z != NULL && y->n < z->n)
-    {
+    while (z != NULL && y->n < z->n) {
         temp = y->n;
         y->n = z->n;
         z->n = temp;
         y = z;
         z = z->parent;
     }
-    cout<<"Key reduced successfully"<<endl;
 }
-/*
- * Delete Nodes in Binomial Heap
- */
-int BinomialHeap::Delete(node* H, int k)
+ 
+int delete_key(struct node* H, int k) 
 {
-    node* np;
-    if (H == NULL)
-    {
-        cout<<"\nHEAP EMPTY!!!!!";
+    struct node* np;
+    if (H == NULL) {
+        printf("\nheap is empty");
         return 0;
     }
-    Decrease_key(H, k, -1000);
-    np = Extract_Min(H);
+ 
+    decrease_key(H, k, -1000);
+    np = extract_min_node(H);
     if (np != NULL)
-        cout<<"Node Deleted Successfully"<<endl;
+        printf("\nnode deleted");
 }
-/*
- * Main Contains Menu
- */
-int main()
-{
-    int n, m, l, i;
-    BinomialHeap bh;
-    node* p;
-    node *H;
-    H = bh.Initializeheap();
-    char ch;
-    while (1)
+
+void printHeap(struct node *H) 
+{ 
+    while(H!=NULL)
     {
-        cout<<"----------------------------"<<endl;
-        cout<<"Operations on Binomial heap"<<endl;
-        cout<<"----------------------------"<<endl;
-        cout<<"1)Insert Element in the heap"<<endl;
-        cout<<"2)Extract Minimum key node"<<endl;
-        cout<<"3)Decrease key of a node"<<endl;
-        cout<<"4)Delete a node"<<endl;
-        cout<<"5)Display Heap"<<endl;
-        cout<<"6)Exit"<<endl;
-        cout<<"Enter Your Choice: ";
-        cin>>l;
-        switch(l)
-        {
+      
+            printTree(H); 
+        
+
+        H=H->sibling; 
+    }
+} 
+
+int main() {
+    int i, n, m, option,nkey;
+    struct node* p;
+    struct node* np;
+    char ch;
+
+  // cout<<x;
+        np = create_node(x);
+        H = insert(H, np);
+         mm=mm+1;
+}
+    // cout<<"enter initial number of elements :";
+   // cin>>n;
+    //cout<<"enter elements :\n";
+    //for (i = 1; i <= n; i++) 
+    //{
+       // cin>>m;
+       //cout<<i<<" ";
+      
+    //}
+
+    display_root_elements(H);
+    printTree(H);
+    
+    do 
+    {
+        cout<<"Enter choice: \n";
+        cout<<"1)insert 2)extract min key 3)decrease key 4)delete 5)quit 6)print heap \n";
+        cin>>option;
+        switch (option) {
         case 1:
-            cout<<"Enter the element to be inserted: ";
-            cin>>m;
-            p = bh.Create_node(m);
-            H = bh.Insert(H, p);
-            break;
+            do {
+                cout<<"enter element:";
+                cin>>m;
+                p = create_node(m);
+                H = insert(H, p);
+                display_root_elements(H);
+                cout<<"cuurent heap is: ";
+                printTree(H);
+                cout<<endl;
+                cout<<"\ninsert more(y): \n";
+                // fflush(stdin);
+                cin>>ch;
+            } while (ch == 'Y' || ch == 'y');
+         break;
         case 2:
-            p = bh.Extract_Min(H);
-            if (p != NULL)
-                cout<<"The node with minimum key: "<<p->n<<endl;
-            else
-                cout<<"Heap is empty"<<endl;
+            do {
+                p = extract_min_node(H);
+                if (p != NULL)
+                    cout<<"\nextracted min node is : "<<p->n;
+                display_root_elements(H);
+                cout<<"cuurent heap is: ";
+                printTree(H);
+                cout<<endl;
+                cout<<"\nextract more(y): \n";
+                cin>>ch;
+            } while (ch == 'Y' || ch == 'y');
             break;
         case 3:
-            cout<<"Enter the key to be decreased: ";
-            cin>>m;
-            cout<<"Enter new key value: ";
-            cin>>l;
-            bh.Decrease_key(H, m, l);
+            do {
+                cout<<"enter old key : ";
+                cin>>m;
+                cout<<"enter new key : ";
+                cin>>nkey;
+                decrease_key(H, m, nkey);
+                cout<<"root elements :\n ";
+                display_root_elements(H);
+                cout<<"cuurent heap is: ";
+                printTree(H);
+                cout<<endl;
+                cout<<"\ndecrease key more(y): \n";
+                cin>>ch;
+            } while (ch == 'Y' || ch == 'y');
             break;
         case 4:
-            cout<<"Enter the key to be deleted: ";
-            cin>>m;
-            bh.Delete(H, m);
+            do {
+                cout<<"enter key : ";
+                cin>>m;
+                delete_key(H, m);
+                display_root_elements(H);
+                cout<<"cuurent heap is: ";
+                printTree(H);a
+                cout<<endl;
+                printf("\ndelete more(y/Y)\n");
+
+                //fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'Y');
             break;
         case 5:
-            cout<<"The Heap is: "<<endl;
-            bh.Display(H);
+            printf("\nexited\n");
             break;
         case 6:
-            exit(1);
+                cout<<"cuurent heap is: ";
+                printTree(H);
+                cout<<endl;
+             break;
         default:
-            cout<<"Wrong Choice";
-	  }
-    }
-    return 0;
+            printf("\nwrong choice\n");
+        }
+    } while (option!= 5);
 }
